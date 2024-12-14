8023e01af0e679673353075a9d7ef3a42c6d81dc
An4nasik <144912844+An4nasik@users.noreply.github.com>
2024-12-14 14:47:54 +0300

    Add files via upload

--- get_data.py
+++ get_data.py
@@ -1,13 +1,14 @@
 from requests import request, Response
 from decouple import config
-
+from json import dumps
 
 class CardReader:
     def __init__(self, url: str, method: str) -> None:
         self.url = url #url куда мы направляем запрос
         self.method = method #метод этого запроса
 
-    def read_data(self, n: int, photo: bool = False) -> Response:
+
+    def read_data(self, n: int, photo: int = 0) -> Response.json:
         api_key = {"Authorization": config("API_KEY")}
         # создается специальный параметр для запроса к товарам
         # он используется в моем предполагаемом методе получения данных
@@ -21,6 +22,6 @@ class CardReader:
                 }
             }
         }
-        dt = request(self.method, self.url, headers=api_key, params=cursor)
-        return dt
+        dt = request(self.method, self.url, headers=api_key, data=dumps(cursor))
+        return dt.json()
 
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import requests
+from json import dumps
 from get_data import CardReader
+from decouple import config
 from sheets import SheetWorker
-
+from requests   import request
 
 scp = ['https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive"]
 work = CardReader("https://content-api.wildberries.ru/content/v2/get/cards/list", "post")
-data = work.read_data(1, False)
-print(data)
-
-sheet = SheetWorker("asd.json", scp, "TestTable")
-sheet.insert_data([1])
-sheet.insert_data([2])
-sheet.insert_data([3])
-sheet.insert_data([4])
+data = work.read_data(10, 0)
+print(data["cards"][0]["nmID"])
+print(data["cards"][0]["subjectName"])
+print(data["cards"][0]["vendorCode"])
+print(data["cards"][0]["createdAt"])
+sheet = SheetWorker("asd.json", scp, "Table")
+for x in data["cards"]:
+    sheet.insert_data([x["nmID"], x["subjectName"], x["vendorCode"], x["createdAt"]])
+# сслыка на таблицу https://docs.google.com/spreadsheets/d/18BdtQVtbsUpbobfCe6e8m7_ZEiBt-CfEEoqfFEb2mjM/edit?usp=sharing
